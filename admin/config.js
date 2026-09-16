d7e4ab80a91d5fe302f3f42a6327dfa6c9c7b09b
davidegolisano-design <davide.golisano@gmail.com>
2026-09-16 14:58:33 +0200

    admin: simplify DEV config to existing Supabase backend

--- admin/config.js
+++ admin/config.js
@@ -1,6 +1,6 @@
 window.LIVEASTA_ADMIN_CONFIG = Object.freeze({
   supabaseUrl: 'https://qvkembahfeecfpsshepv.supabase.co',
   supabasePublishableKey: 'sb_publishable_RmHnQtTjnJqEwNobq_H4bw_D9tL84Ex',
-  adminFunctionName: 'liveasta-admin-api',
-  version: 'admin-mvp-0.1.0'
+  version: 'admin-dev-0.2.0',
+  pollIntervalMs: 10000
 });
