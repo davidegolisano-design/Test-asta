e4828c1cdafb849a5a80ed4b1845763f0dd007cc
davidegolisano-design <davide.golisano@gmail.com>
2026-09-16 15:02:10 +0200

    admin: bump PWA cache for polling update

--- admin/sw.js
+++ admin/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'liveasta-admin-dev-0.2.0';
+const CACHE_NAME = 'liveasta-admin-dev-0.2.1';
 const APP_SHELL = ['./','./index.html','./styles.css','./config.js','./app.js','./manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
