4d2f0305805e77d00906ea2e09319bda8f88fa75
davidegolisano-design <davide.golisano@gmail.com>
2026-09-16 15:01:56 +0200

    admin: reduce pending-room polling to 5 seconds

--- admin/config.js
+++ admin/config.js
@@ -1,6 +1,6 @@
 window.LIVEASTA_ADMIN_CONFIG = Object.freeze({
   supabaseUrl: 'https://qvkembahfeecfpsshepv.supabase.co',
   supabasePublishableKey: 'sb_publishable_RmHnQtTjnJqEwNobq_H4bw_D9tL84Ex',
-  version: 'admin-dev-0.2.0',
-  pollIntervalMs: 10000
+  version: 'admin-dev-0.2.1',
+  pollIntervalMs: 5000
 });
