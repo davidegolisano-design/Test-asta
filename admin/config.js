81e3be307ef4101ba74cc2c833ac5735bd46eb99
davidegolisano-design <davide.golisano@gmail.com>
2026-09-16 14:45:13 +0200

    admin: add isolated PWA config

---
+++ admin/config.js
@@ -0,0 +1,6 @@
+window.LIVEASTA_ADMIN_CONFIG = Object.freeze({
+  supabaseUrl: 'https://qvkembahfeecfpsshepv.supabase.co',
+  supabasePublishableKey: 'sb_publishable_RmHnQtTjnJqEwNobq_H4bw_D9tL84Ex',
+  adminFunctionName: 'liveasta-admin-api',
+  version: 'admin-mvp-0.1.0'
+});
